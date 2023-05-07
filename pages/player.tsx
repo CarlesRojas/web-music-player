786f714bad75602162835fca116312a5367a5d5a
CarlesRojas <carlesrojas@outlook.com>
2023-05-07 23:59:25 +0200

    All working!

--- pages/player.tsx
+++ pages/player.tsx
@@ -2,14 +2,14 @@ import MobileLayout from '@/components/MobileLayout';
 import { useSpotifyLoginData } from '@/shared/spotify/login/useSpotifyLoginData';
 import { usePlayer } from '@/shared/spotify/query/usePlayer';
 import { useUserProfile } from '@/shared/spotify/query/useUserProfile';
-import { useQueryClient } from '@tanstack/react-query';
 import Head from 'next/head';
 
 export default function Player() {
   const { spotifyLoginData } = useSpotifyLoginData();
   const { userProfile } = useUserProfile();
   const { player } = usePlayer();
-  const queryClient = useQueryClient();
+
+  console.log(spotifyLoginData, userProfile, player);
 
   return (
     <>
@@ -17,15 +17,6 @@ export default function Player() {
         <title>Spot</title>
         <meta name="description" content="Web based client for listening to Spotify." />
       </Head>
-
-      {/* <button
-        type="button"
-        onClick={async () => {
-          queryClient.invalidateQueries(['usePlayer']);
-        }}
-      >
-        Play
-      </button> */}
 
       <MobileLayout />
     </>
