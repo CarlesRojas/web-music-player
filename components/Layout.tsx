0e8d6e53dfb1f2a0c3e902223d00c388169805e7
CarlesRojas <carlesrojas@outlook.com>
2023-05-07 12:17:56 +0200

    betetr height anim

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,7 +3,7 @@
 import useResize from '@/hooks/useResize';
 import { clamp } from '@/shared/interpolate';
 import { useDrag } from '@use-gesture/react';
-import { motion, useMotionValue, useTransform } from 'framer-motion';
+import { motion, useMotionValue } from 'framer-motion';
 import { useState } from 'react';
 import Controls from './Controls';
 import Library from './Library';
@@ -13,21 +13,14 @@ export default function Layout() {
   const [limits, setLimits] = useState({ min: 0.25 * window.innerWidth, max: window.innerWidth });
   useResize(() => setLimits({ min: 0.25 * window.innerWidth, max: window.innerWidth }));
 
-  console.log(limits);
-
-  let height = useMotionValue(0);
-  let x = useMotionValue(0);
-  let y = useMotionValue(0);
+  let height = useMotionValue(limits.max);
+  // let x = useMotionValue(0);
+  // let y = useMotionValue(0);
 
   const bindDrag = useDrag(({ active, movement: [mx, my], direction: [, yDir], velocity }) => {
-    let yDisplacement = clamp((Math.abs(my) * 2) / window.innerHeight);
-
-    // const newHeight = lerp(CONTROLS_HEIGHT_LIMIT_IN_VW.max, CONTROLS_HEIGHT_LIMIT_IN_VW.min, yDisplacement);
-    // console.log(yDisplacement, newHeight);
-
-    height.set(yDisplacement);
-    x.set(mx);
-    y.set(my);
+    height.set(clamp(limits.max - my, limits.min, limits.max));
+    // x.set(mx);
+    // y.set(my);
   });
 
   return (
@@ -36,12 +29,7 @@ export default function Layout() {
         <Library />
       </motion.div>
 
-      <motion.div
-        className="w-full"
-        style={{
-          height: useTransform(height, [0, 1], [limits.max, limits.min])
-        }}
-      >
+      <motion.div className="w-full" style={{ height }}>
         <Controls />
       </motion.div>
 
