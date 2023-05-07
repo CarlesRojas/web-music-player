28ed8f690df7fa3df68f9c6ca33a05bb31d7a1ef
CarlesRojas <carlesrojas@outlook.com>
2023-05-07 23:56:12 +0200

    working player hook

--- pages/player.tsx
+++ pages/player.tsx
@@ -1,29 +1,15 @@
 import MobileLayout from '@/components/MobileLayout';
 import { useSpotifyLoginData } from '@/shared/spotify/login/useSpotifyLoginData';
+import { usePlayer } from '@/shared/spotify/query/usePlayer';
 import { useUserProfile } from '@/shared/spotify/query/useUserProfile';
+import { useQueryClient } from '@tanstack/react-query';
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
-import SpotifyPlayer from 'spotify-web-playback';
 
 export default function Player() {
   const { spotifyLoginData } = useSpotifyLoginData();
   const { userProfile } = useUserProfile();
-
-  console.log(userProfile);
-
-  const [player, setPlayer] = useState<SpotifyPlayer | null>(null);
-
-  useEffect(() => {
-    if (!spotifyLoginData) return;
-
-    const loadSpotifyPlayer = async () => {
-      const spotify = new SpotifyPlayer('Spot', 0.5);
-      // await spotify.connect(spotifyLoginData.accessToken);
-      setPlayer(spotify);
-    };
-
-    loadSpotifyPlayer();
-  }, [spotifyLoginData]);
+  const { player } = usePlayer();
+  const queryClient = useQueryClient();
 
   return (
     <>
@@ -32,14 +18,14 @@ export default function Player() {
         <meta name="description" content="Web based client for listening to Spotify." />
       </Head>
 
-      <button
+      {/* <button
         type="button"
         onClick={async () => {
-          spotifyLoginData && (await player?.connect(spotifyLoginData.accessToken));
+          queryClient.invalidateQueries(['usePlayer']);
         }}
       >
         Play
-      </button>
+      </button> */}
 
       <MobileLayout />
     </>
---
+++ shared/spotify/query/usePlayer.ts
@@ -0,0 +1,61 @@
+import { useQuery } from '@tanstack/react-query';
+import { useEffect, useRef, useState } from 'react';
+import SpotifyPlayer from 'spotify-web-playback';
+import { z } from 'zod';
+import { useSpotifyLoginData } from '../login/useSpotifyLoginData';
+
+const PlayerSchema = z.object({
+  name: z.string(),
+  email: z.string(),
+  image: z.string().optional()
+});
+
+export type Player = z.infer<typeof PlayerSchema>;
+
+const getPlayer = async (accessToken?: string) => {
+  if (!accessToken) throw new Error('No access token provided');
+
+  const player = new SpotifyPlayer('Spot', 0.5);
+
+  try {
+    await player.connect(accessToken);
+  } catch (error) {
+    console.log(error);
+    throw new Error('Could not connect to Spotify');
+  }
+
+  return player;
+};
+
+export const usePlayer = () => {
+  const { spotifyLoginData } = useSpotifyLoginData();
+
+  const [interacted, setInteracted] = useState(false);
+  const intervalRef = useRef<NodeJS.Timeout>();
+
+  useEffect(() => {
+    intervalRef.current = setInterval(async () => {
+      const audio = new Audio('/audio.mp3');
+
+      try {
+        await audio.play();
+        setInteracted(true);
+        clearInterval(intervalRef.current);
+      } catch (error) {}
+    }, 500);
+
+    return () => {
+      clearInterval(intervalRef.current!);
+    };
+  }, []);
+
+  const {
+    data: player,
+    isError: isErrorPlayer,
+    isLoading: isLodingPlayer
+  } = useQuery(['usePlayer'], () => getPlayer(spotifyLoginData?.accessToken), {
+    enabled: !!spotifyLoginData?.accessToken && interacted
+  });
+
+  return { player, isErrorPlayer, isLodingPlayer };
+};
