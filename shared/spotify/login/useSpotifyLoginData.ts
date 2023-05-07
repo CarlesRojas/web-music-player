50c80fbdcb81f42283eeecc8db4b1175807d91a8
CarlesRojas <carlesrojas@outlook.com>
2023-05-07 20:32:05 +0200

    get login data with a query hook

--- shared/spotify/login/useSpotifyLoginData.ts
+++ shared/spotify/login/useSpotifyLoginData.ts
@@ -1,45 +1,65 @@
+import { env } from '@/env.mjs';
 import { LocalStorageKey, Route } from '@/shared/constants';
+import { useQuery } from '@tanstack/react-query';
+import axios, { AxiosError } from 'axios';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 import { SpotifyLoginData, SpotifyLoginDataSchema } from './useGetSpotifyLoginData';
-import { useRefreshSpotifyLoginData } from './useRefreshSpotifyLoginData';
+
+export interface RefreshSpotifyTokenMutation {
+  refreshToken?: string;
+}
+
+export const refreshSpotifyLoginData = async ({ refreshToken }: RefreshSpotifyTokenMutation) => {
+  if (!refreshToken) throw new AxiosError('Missing refreshToken');
+
+  return axios({
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization:
+        'Basic ' +
+        Buffer.from(`${env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID}:${env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET}`).toString('base64')
+    },
+    url: `${env.NEXT_PUBLIC_SPOTIFY_API_BASE_URL}/api/token`,
+    data: {
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken,
+      client_id: env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID
+    }
+  });
+};
+
+const getSpotifyLoginData = async () => {
+  const rawLoginData = localStorage.getItem(LocalStorageKey.SPOTIFY_LOGIN_DATA) as string;
+  let loginData = SpotifyLoginDataSchema.parse(JSON.parse(rawLoginData));
+
+  if (loginData.expireDate < Date.now()) {
+    const response = await refreshSpotifyLoginData({ refreshToken: loginData.refreshToken });
+    const refreshedSpotifyLoginData: SpotifyLoginData = SpotifyLoginDataSchema.parse({
+      accessToken: response.data.access_token,
+      refreshToken: response.data.refresh_token,
+      expireDate: Date.now() + response.data.expires_in * 1000
+    });
+    loginData = refreshedSpotifyLoginData;
+  }
+
+  localStorage.setItem(LocalStorageKey.SPOTIFY_LOGIN_DATA, JSON.stringify(loginData));
+  return loginData;
+};
 
 export const useSpotifyLoginData = () => {
   const { push } = useRouter();
-  const [spotifyLoginData, setSpotifyLoginData] = useState<SpotifyLoginData | null>(null);
 
-  const { refreshedSpotifyLoginData, refreshSpotifyLoginDataMutation, isErrorRefreshSpotifyToken } =
-    useRefreshSpotifyLoginData();
+  const {
+    data: spotifyLoginData,
+    isError: isErrorSpotifyLoginData,
+    isLoading: isLodingSpotifyLoginData
+  } = useQuery(['spotifyLoginData'], () => getSpotifyLoginData());
 
-  useEffect(() => {
-    try {
-      const rawLoginData = localStorage.getItem(LocalStorageKey.SPOTIFY_LOGIN_DATA) as string;
-      const loginData = SpotifyLoginDataSchema.parse(JSON.parse(rawLoginData));
-      setSpotifyLoginData(loginData);
-    } catch (error) {
-      localStorage.clear();
-      push(Route.HOME);
-    }
-  }, [push]);
-
-  useEffect(() => {
-    if (!spotifyLoginData) return;
-
-    if (spotifyLoginData.expireDate < Date.now())
-      refreshSpotifyLoginDataMutation({ refreshToken: spotifyLoginData.refreshToken });
-  }, [refreshSpotifyLoginDataMutation, spotifyLoginData]);
-
-  useEffect(() => {
-    if (!refreshedSpotifyLoginData) return;
-
-    setSpotifyLoginData(refreshedSpotifyLoginData);
-  }, [refreshedSpotifyLoginData]);
-
-  useEffect(() => {
-    if (!isErrorRefreshSpotifyToken) return;
+  if (isErrorSpotifyLoginData) {
     localStorage.clear();
     push(Route.HOME);
-  }, [isErrorRefreshSpotifyToken, push]);
+  }
 
-  return spotifyLoginData;
+  return { spotifyLoginData, isLodingSpotifyLoginData };
 };
