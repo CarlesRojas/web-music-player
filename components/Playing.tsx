970fe3bd76de519b9999be0716bf79e4bc7aa9ec
CarlesRojas <carlesrojas@outlook.com>
2023-05-13 10:32:07 +0200

    show song name and artist

--- components/Playing.tsx
+++ components/Playing.tsx
@@ -1,3 +1,35 @@
+import { usePlaybackState } from '@/shared/spotify/query/usePlaybackState';
+import { ReactNode } from 'react';
+import { RiComputerLine, RiMicLine, RiPlayListLine } from 'react-icons/ri';
+
 export default function Playing() {
-  return <main className="p-2 relative w-full h-full flex items-center justify-center"></main>;
+  const { playbackState } = usePlaybackState();
+
+  const container = (children?: ReactNode) => (
+    <main className="p-2 relative w-full h-full flex flex-col items-center justify-center">
+      <div className="relative w-full grow flex flex-col items-center">{children}</div>
+      <div className="relative w-full flex justify-between">
+        <RiComputerLine className="h-12 w-12 p-3" />
+        <RiMicLine className="h-12 w-12 p-3" />
+        <RiPlayListLine className="h-12 w-12 p-3" />
+      </div>
+    </main>
+  );
+
+  if (!playbackState) return container();
+
+  const { item } = playbackState;
+  const { album, name, artists } = item;
+
+  return container(
+    <>
+      <h2 className="w-full text-center text-xl text-ellipsis overflow-hidden whitespace-nowrap">{name}</h2>
+
+      {artists.length > 0 && (
+        <h3 className="w-full text-center text-base opacity-60 text-ellipsis overflow-hidden whitespace-nowrap">
+          {artists[0].name}
+        </h3>
+      )}
+    </>
+  );
 }
